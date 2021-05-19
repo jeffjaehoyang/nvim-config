7a5ba6b5fed3bf0781916bf2a99f8ccc14106700
Jeff Yang <jho.yang96@gmail.com>
2021-05-19 16:26:32 -0500

    no style linting for python setup

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -1,5 +1,18 @@
 lspconfig = require'lspconfig'
 
-lspconfig.pyls.setup{}
+lspconfig.pyls.setup{
+    settings = {
+        pyls = {
+            plugins = {
+                pycodestyle = {
+                    enabled = false,
+                    ignore = {
+                        "E501"
+                    }
+                }
+            }
+        }
+    }
+}
 lspconfig.tsserver.setup{}
 lspconfig.rust_analyzer.setup{}
