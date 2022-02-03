14037a16072a347c9adb2a424eaeb0bf8968ffc1
Jeff Yang <jho.yang96@gmail.com>
2022-02-03 13:32:14 -0600

    stuff

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -86,11 +86,7 @@ end
 local diagnostics_config = {
     virtual_text = {
         spacing = 4,
-        --prefix = "⬢ "
-        source = true,
         prefix = "■ "
-        --prefix = "⬣ ",
-        --prefix = "» "
     },
     -- show signs
     signs = {
